41d64d7bde87e7e3dfd0c74d981b264dbebae3d0
Rusmansyah Putra NH <rusman.putra.712@gmail.com>
2026-08-11 19:43:47 +0700

    feat: SEO and performance optimization
    - Create dynamic XML sitemap controller and view for Google Search Console
    - Add sitemap.xml routing
    - Add Google Analytics manual tracking code to main layout
    - Provide .htaccess configuration for Gzip compression and browser caching

--- application/views/sitemap.php
+++ application/views/sitemap.php
@@ -1,60 +1,75 @@
-<?php echo '<?xml version="1.0" encoding="UTF-8"?>' . "\n"; ?>
+<?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 
+    <!-- HALAMAN UTAMA / STATIS -->
     <url>
         <loc><?= base_url() ?></loc>
+        <changefreq>weekly</changefreq>
         <priority>1.0</priority>
     </url>
     <url>
         <loc><?= base_url('journey') ?></loc>
+        <changefreq>weekly</changefreq>
         <priority>0.9</priority>
-    </url>
-    <url>
-        <loc><?= base_url('fashion') ?></loc>
-        <priority>0.9</priority>
-    </url>
-    <url>
-        <loc><?= base_url('gallery') ?></loc>
-        <priority>0.8</priority>
     </url>
     <url>
         <loc><?= base_url('about') ?></loc>
+        <changefreq>monthly</changefreq>
         <priority>0.8</priority>
     </url>
     <url>
         <loc><?= base_url('journal') ?></loc>
+        <changefreq>daily</changefreq>
         <priority>0.8</priority>
     </url>
     <url>
-        <loc><?= base_url('contact') ?></loc>
+        <loc><?= base_url('fashion') ?></loc>
+        <changefreq>weekly</changefreq>
         <priority>0.7</priority>
     </url>
+    <url>
+        <loc><?= base_url('gallery') ?></loc>
+        <changefreq>weekly</changefreq>
+        <priority>0.7</priority>
+    </url>
+    <url>
+        <loc><?= base_url('contact') ?></loc>
+        <changefreq>monthly</changefreq>
+        <priority>0.6</priority>
+    </url>
 
-    <?php if (!empty($journeys)): ?>
-        <?php foreach ($journeys as $journey): ?>
+    <!-- HALAMAN DINAMIS: JOURNEYS -->
+    <?php if (!empty($journeys)): foreach ($journeys as $item): ?>
             <url>
-                <loc><?= base_url('journey/' . $journey->slug) ?></loc>
-                <priority>0.9</priority>
-            </url>
-        <?php endforeach; ?>
-    <?php endif; ?>
-
-    <?php if (!empty($fashions)): ?>
-        <?php foreach ($fashions as $fashion): ?>
-            <url>
-                <loc><?= base_url('fashion/' . $fashion->slug) ?></loc>
+                <loc><?= base_url('journey/' . $item->slug) ?></loc>
+                <lastmod><?= date('Y-m-d\TH:i:sP', strtotime($item->updated_at ?? $item->created_at)) ?></lastmod>
+                <changefreq>weekly</changefreq>
                 <priority>0.8</priority>
             </url>
-        <?php endforeach; ?>
-    <?php endif; ?>
+    <?php endforeach;
+    endif; ?>
 
-    <?php if (!empty($journals)): ?>
-        <?php foreach ($journals as $journal): ?>
+    <!-- HALAMAN DINAMIS: JOURNALS -->
+    <?php if (!empty($journals)): foreach ($journals as $item): ?>
             <url>
-                <loc><?= base_url('journal/' . $journal->slug) ?></loc>
+                <loc><?= base_url('journal/' . $item->slug) ?></loc>
+                <lastmod><?= date('Y-m-d\TH:i:sP', strtotime($item->updated_at ?? $item->created_at)) ?></lastmod>
+                <changefreq>monthly</changefreq>
                 <priority>0.7</priority>
             </url>
-        <?php endforeach; ?>
-    <?php endif; ?>
+    <?php endforeach;
+    endif; ?>
+
+    <!-- HALAMAN DINAMIS: FASHIONS -->
+    <?php if (!empty($fashions)): foreach ($fashions as $item): ?>
+            <url>
+                <loc><?= base_url('fashion/' . $item->slug) ?></loc>
+                <!-- Asumsi tabel fashion punya created_at. Hapus baris lastmod ini jika di DB tidak ada kolom tanggal -->
+                <lastmod><?= date('Y-m-d\TH:i:sP', strtotime($item->created_at)) ?></lastmod>
+                <changefreq>monthly</changefreq>
+                <priority>0.6</priority>
+            </url>
+    <?php endforeach;
+    endif; ?>
 
 </urlset>
